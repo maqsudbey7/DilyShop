3645403d40831a5ce74936c61f42e7f0587ed453
unknown <maqsudbekyusubov50@gmail.com>
2025-08-08 16:27:08 +0500

    O'zgartirish kiritildi

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-  fontFamily: {
-    sans: ['Poppins', 'sans-serif'],
-  },
-  
-})
+  plugins: [react()],
+  base: '/', // Netlify uchun to'g'ri base
+});
