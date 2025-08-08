ceb5803d6fe594bac55293f9c05c020b31e9b1c4
unknown <maqsudbekyusubov50@gmail.com>
2025-08-08 16:42:44 +0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Netlify uchun to'g'ri base
+  base: './', // barcha fayllarni index.html joylashgan joydan qidiradi
 });
