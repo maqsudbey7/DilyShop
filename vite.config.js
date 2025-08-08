dcaa11a3349ecaf3e79bbde604cb88fd7005e04b
unknown <maqsudbekyusubov50@gmail.com>
2025-08-08 16:48:01 +0500

    Fixed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // barcha fayllarni index.html joylashgan joydan qidiradi
+  base: './', // bu yerda ./ bo‘lishi kerak
 });
